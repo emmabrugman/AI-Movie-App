ca59f4314c4f67ef15a41b16c5085fa81095ee16
josefalconi <jfalconi@ucsd.edu>
2025-01-31 10:16:30 -0800

    basic app working, autofill for search not working

---
+++ AI-Movie-App/src/services/Api.js
@@ -0,0 +1,18 @@
+const API_KEY = "355db2a25648a813a876b3fd5340e35f";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
